b27703d45ad193a8af657c3c9731cee7facc6922
KNI-0830 <ayuayugonbe@gmail.com>
2026-03-22 19:32:27 +0900

    feat:個別にpidゲインを調整できる関数を追加。

--- CAN/app/src/app.cpp
+++ CAN/app/src/app.cpp
@@ -5,33 +5,29 @@
 #include "can.h"
 #include "gpio.h"
 
-robomas_can can;
-pid_controller pid;
 MotorCurrent_can1 current_1_data;
 MotorCurrent_can2 current_2_data;
 MotorCurrent_can1* current_1 = &current_1_data;
 MotorCurrent_can2* current_2 = &current_2_data;
+
+robomas_can can;
+pid_controller pid_1;
+pid_controller pid_2;
 
 extern "C" void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef* hcan)
 {
     can.can_callback(hcan);
 }
 
-void packet_current_data()
-{
-    can.can1_current(16000, 0, 0, 0);
-    can.can2_current(0, 0, 0, 0);
-}
-
 void setup()
 {
     can.init(&hcan1);
     can.init(&hcan2);
-    HAL_GPIO_WritePin(GPIOF, GPIO_PIN_14, GPIO_PIN_SET);  // LED_G 常時ON
+    pid_1.set_gain(2.0f, 0.03f, 0.3f);
 }
 void loop()
 {
-    can.can1_current(pid.pid_controll(&can.feedback[0], 10000), 0, 0, 0);
+    can.can1_current(pid_1.pid_controll(&can.feedback[0], 10000), 0, 0, 0);
     can.send_data(&hcan1, M_SEND_CANID, (uint8_t*)current_1, 8);
     HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_11);
 
